990f4d8dc26780b2387a9612a1349d8155c09aae
JunaidRajah <80385655+JunaidRajah@users.noreply.github.com>
2021-11-17 00:46:40 +0200

    Initial Setup

---
+++ UnmaskedEngine/UnmaskedEngine.h
@@ -0,0 +1,18 @@
+//
+//  UnmaskedEngine.h
+//  UnmaskedEngine
+//
+//  Created by Junaid Rajah on 2021/11/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnmaskedEngine.
+FOUNDATION_EXPORT double UnmaskedEngineVersionNumber;
+
+//! Project version string for UnmaskedEngine.
+FOUNDATION_EXPORT const unsigned char UnmaskedEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnmaskedEngine/PublicHeader.h>
+
+
