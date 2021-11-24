6f5f021f53c50c7dff2ccebdc190e473ff46ac5e
Junaid Rajah <junaidrajah786@gmail.com>
2021-11-24 10:23:10 +0200

    Made requested changes on main app affecting current

--- UnmaskedEngine/View Models/UnmaskedViewModel.swift
+++ UnmaskedEngine/View Models/UnmaskedViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum HeroInfoGroup: String, CaseIterable {
-    case powerstats = "Power Stats"
+    case powerStats = "Power Stats"
     case biography = "Biography"
     case appearance = "Appearance"
     case work = "Work History"
@@ -18,8 +18,8 @@ public enum HeroInfoGroup: String, CaseIterable {
 public class UnmaskedViewModel {
     
     private weak var delegate: UnmaskedViewModelDelegate?
-    private var hero: SuperheroResponseModel?
-    public var currentInfoGroup: HeroInfoGroup = .powerstats
+    private var hero: SuperheroResponseModel
+    public var currentInfoGroup: HeroInfoGroup = .powerStats
     
     public var heroInfoGroup: HeroInfoGroup {
         get {
@@ -38,7 +38,7 @@ public class UnmaskedViewModel {
     
     public func nextGroup() {
         switch currentInfoGroup {
-        case .powerstats:
+        case .powerStats:
             currentInfoGroup = .biography
         case .biography:
             currentInfoGroup = .appearance
@@ -47,12 +47,32 @@ public class UnmaskedViewModel {
         case .work:
             currentInfoGroup = .connections
         case .connections:
-            currentInfoGroup = .powerstats
+            currentInfoGroup = .powerStats
         }
         delegate?.changeGroup()
     }
     
-    public var chosenHero: SuperheroResponseModel? {
+    public var chosenHero: SuperheroResponseModel {
         hero
+    }
+    
+    public var powerstats: PowerStats {
+        hero.powerstats
+    }
+    
+    public var biography: Biography {
+        hero.biography
+    }
+    
+    public var appearance: Appearance {
+        hero.appearance
+    }
+    
+    public var work: Work {
+        hero.work
+    }
+    
+    public var connections: Connections {
+        hero.connections
     }
 }
